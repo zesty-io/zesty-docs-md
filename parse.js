75fa25c139f260d69b14b36baf3c8e6ef5451b89
darwin808 <darwin.apolinario12@gmail.com>
2023-06-22 00:01:19 +0800

    feat:init

--- parse.js
+++ parse.js
@@ -26,13 +26,33 @@ const readAllMarkdownFilesRecursive = (directoryPath) => {
    }
 }
 
+const convertBrokenReferenceToLink = (filePath) => {
+   try {
+      const fileContent = fs.readFileSync(filePath, "utf-8")
+      const modifiedContent = fileContent
+         .replaceAll(
+            /\{% content-ref url="([^"]+)" %\}\s*\[([^\]]+)\]\([^)]+\)\s*\{% endcontent-ref %\}/g,
+            "[$2]($1)",
+         )
+         .replaceAll(/\{% hint style="([\w\s]+)" %\}\s*(.*?)\s*\{% endhint %\}/gs, "> $2")
+
+      const newFilePath = `${filePath}.copy.md`
+      fs.writeFileSync(newFilePath, modifiedContent, "utf-8")
+      //   fs.writeFileSync(filePath, modifiedContent, "utf-8")
+      console.log(`Modified file saved: ${filePath}`)
+   } catch (error) {
+      console.error(`Error converting and saving file: ${error}`)
+   }
+}
+
 // Example usage
-const directoryPath = "."
+const directoryPath = "./accounts/guides/accounts-api"
 const markdownFiles = readAllMarkdownFilesRecursive(directoryPath)
 if (markdownFiles) {
    markdownFiles.forEach((file) => {
       console.log(`File Path: ${file.filePath}`)
       //   console.log(`Content: ${file.content}`)
       console.log("-----------------------")
+      convertBrokenReferenceToLink(file.filePath)
    })
 }
